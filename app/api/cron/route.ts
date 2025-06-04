78eecca4b9f2c1c1969274c290f36b80f5d70a1b
Matthew <matthewaureliustjoa@gmail.com>
2025-06-04 19:04:56 -0400

    Changed cron to run every 1 hour

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,15 +1,15 @@
 import sql from '@/app/lib/neon';
 import { NextRequest, NextResponse } from 'next/server';
 async function checkUptimeForAllUsers() {
-    const websites = await sql`SELECT * FROM websites WHERE is_active = true`;
+  const websites = await sql`SELECT * FROM websites WHERE is_active = true`;
 
-    for (const website of websites) {
-        try {
-            const start = Date.now();
-            const res = await fetch(website.url, { method: 'GET' });
-            const time = Date.now() - start;
+  for (const website of websites) {
+    try {
+      const start = Date.now();
+      const res = await fetch(website.url, { method: 'GET' });
+      const time = Date.now() - start;
 
-            await sql`
+      await sql`
         INSERT INTO uptime_checks (website_id, status, response_time, status_code)
         VALUES (
           ${website.id},
@@ -18,19 +18,24 @@ async function checkUptimeForAllUsers() {
           ${res.status}
         )
       `;
-        } catch (error) {
-            await sql`
+    } catch (error) {
+      await sql`
         INSERT INTO uptime_checks (website_id, status, error_message)
         VALUES (${website.id}, 'down', ${String(error)})
       `;
-        }
     }
+  }
 }
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
-    if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return NextResponse.json({ "Rejected": "You are not authorized" }, { status: 401 })
-    }
-    await checkUptimeForAllUsers();
-    return NextResponse.json({ success: true });
+  if (
+    req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json(
+      { Rejected: 'You are not authorized' },
+      { status: 401 }
+    );
+  }
+  await checkUptimeForAllUsers();
+  return NextResponse.json({ success: true });
 }
